59b845f3fff2039520ae58bc87fb8ec75455e0ec
Olly Ewert <oliver.ewert@control-plane.io>
2022-06-09 16:51:59 +1200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+CLIENT_VERSION = "0.1"
+PACKAGE_NAME = "kubernetes_leaderelection_asyncio"
+
+with open('requirements.txt') as f:
+    REQUIRES = f.readlines()
+
+setup(
+    name=PACKAGE_NAME,
+    version=CLIENT_VERSION,
+    description="Leader electrion for kubernetes asyncio client",
+    author_email="",
+    author="",
+    license="Apache License Version 2.0",
+    url="https://github.com/ollytheninja/kubernetes_leaderelection_asyncio",
+    keywords=[],
+    install_requires=REQUIRES,
+    packages=['leaderelection'],
+    include_package_data=True,
+    long_description="""\
+        a copy of the leader election code from the Official Python client library for kubernetes to make it compatible with it's not so official asyncio counterpart.     
+    """,
+)
